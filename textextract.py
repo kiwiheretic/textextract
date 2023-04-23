7d191e1cce55867817a184757dd3fd4d2c91af76
kiwiheretic <kiwiheretic@myself.com>
2023-04-23 15:10:41 +1200

    fix bug in row/column data

--- textextract.py
+++ textextract.py
@@ -1,13 +1,15 @@
-from PIL import Image
+from PIL import Image, ImageDraw
 import os
 
 WHITE=255
 
 im = Image.open(os.path.join('GriffithsQM-Images', 'Griffiths-000.png') )
+im2 = Image.new('1', im.size, color=1)
 print(im.format, im.size, im.mode)
 width, height = im.size
 data = im.getdata()
 
+draw = ImageDraw.Draw(im2)
 
 cursorx = None
 cursorstart = None
@@ -19,15 +21,19 @@ for ii in range(len(data)):
     if pixel == WHITE:
         if cursorstart is not None:
             row = (cursory, cursorstart, cursorx)
+            draw.line([(cursorstart, cursory), (cursorx, cursory)], fill='black', width=1)
             print (row)
             rowdata.append(row) 
             cursorstart = None
     else: #if pixel != WHITE:
-        cursorx = ii % height
-        cursory = ii // height
+        cursorx = ii % width
+        cursory = ii // width
+        #print (cursorx, cursory)
+        #im2.putpixel((cursorx, cursory), 0)
         if cursorstart is None:
             cursorstart = cursorx
         #print (cursory, cursorstart, cursorx, pixel)
 
 
 print (len(data), width, height)
+im2.save('output.png')
